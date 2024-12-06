b519ad62050a4f982214669bdd1b5d625ac80084
AreebaMehboob1 <fa21-bcs-034@cuiatk.edu.pk>
2024-12-06 11:07:11 +0500

    commit2 HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World! RAMESH IS MY FRIEND");
+        System.out.println(" RAMESH IS MY FRIEND");
     }
 }
